e143131804a3d0f890552c625e79ec3c2e108a2c
Niyaz Mulla <niyazmulla@hotmail.com>
2024-03-14 10:29:34 -0500

    first commit

---
+++ FuncDeploymentDemo/FuncHttp.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace FuncDeploymentDemo
+{
+    public static class FuncHttp
+    {
+        [FunctionName("FuncHttp")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            return new OkObjectResult("Awesome app version 1.2.");
+        }
+    }
+}
