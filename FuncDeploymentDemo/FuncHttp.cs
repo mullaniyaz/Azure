76ee663d0e7f80d0d2f1b6f1542b253dadf4f15f
Niyaz Mulla <niyazmulla@hotmail.com>
2024-03-14 10:31:35 -0500

    updated version to 1.1

--- FuncDeploymentDemo/FuncHttp.cs
+++ FuncDeploymentDemo/FuncHttp.cs
@@ -14,7 +14,7 @@ namespace FuncDeploymentDemo
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            return new OkObjectResult("Awesome app version 1.2.");
+            return new OkObjectResult("Awesome app version 1.1.");
         }
     }
 }
